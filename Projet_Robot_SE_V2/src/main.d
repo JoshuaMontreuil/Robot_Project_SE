3da4c62e99f0d2fef26b92031b20865ad6526b1c
Joshua Montreuil <joshua.montreuil@reseau.eseo.fr>
2023-02-12 11:34:34 +0100

    Robot_V1 & V2 file project separation
    Creation of a project for the V2. Changing the name of the V1 for  "Projet_Robot_SE_V1"

---
+++ Projet_Robot_SE_V2/src/main.d
@@ -0,0 +1,17 @@
+../bin/robot_pc: main.c mrRobot/adminui.h mrRobot/pilot.h \
+ /home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/prose.h \
+ /home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/defs.h \
+ /home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/motors.h \
+ /home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/contact_sensor.h \
+ /home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/light_sensor.h \
+ /home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/prose_brick_pi.h \
+ /home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/prose_infox.h
+mrRobot/adminui.h:
+mrRobot/pilot.h:
+/home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/prose.h:
+/home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/defs.h:
+/home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/motors.h:
+/home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/contact_sensor.h:
+/home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/light_sensor.h:
+/home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/prose_brick_pi.h:
+/home/joshua/Documents/GitHub/Robot_Project_SE/infox_prose-x86_64-v0.3/include/infox/prose/prose_infox.h:
